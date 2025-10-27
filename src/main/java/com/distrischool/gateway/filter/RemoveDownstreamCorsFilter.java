36e29c6a04cfeb11c2134437133f168ab497243c
pmas98 <pmas98@hotmail.com>
2025-10-27 12:53:08 -0300

    Implement CORS handling in RemoveDownstreamCorsFilter by removing the Origin header from requests and cleaning up CORS headers from responses.

--- src/main/java/com/distrischool/gateway/filter/RemoveDownstreamCorsFilter.java
+++ src/main/java/com/distrischool/gateway/filter/RemoveDownstreamCorsFilter.java
@@ -3,9 +3,10 @@ package com.distrischool.gateway.filter;
 import org.springframework.cloud.gateway.filter.GatewayFilterChain;
 import org.springframework.cloud.gateway.filter.GlobalFilter;
 import org.springframework.core.Ordered;
+import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.http.server.reactive.ServerHttpRequestDecorator;
 import org.springframework.stereotype.Component;
 import org.springframework.web.server.ServerWebExchange;
-
 import reactor.core.publisher.Mono;
 
 /**
@@ -18,8 +19,17 @@ public class RemoveDownstreamCorsFilter implements GlobalFilter, Ordered {
 
     @Override
     public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
-        return chain.filter(exchange).then(Mono.fromRunnable(() -> {
-            var response = exchange.getResponse();
+        // Remove Origin header from request to prevent downstream services from rejecting based on CORS
+        ServerHttpRequest modifiedRequest = exchange.getRequest().mutate()
+            .headers(httpHeaders -> httpHeaders.remove("Origin"))
+            .build();
+        
+        ServerWebExchange modifiedExchange = exchange.mutate()
+            .request(modifiedRequest)
+            .build();
+        
+        return chain.filter(modifiedExchange).then(Mono.fromRunnable(() -> {
+            var response = modifiedExchange.getResponse();
             var headers = response.getHeaders();
             
             // Remove all CORS headers from downstream services
